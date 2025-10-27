f16e61afec85b6d8b4d35597836b63f058434741
Dima Kravchenko <dima.kravchenko.1999@gmail.com>
2025-10-27 20:02:58 +0200

    homework

---
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Modal } from "@/components/Modal/Modal";
+import { fetchNoteById } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import type { Note } from "@/types/note";
+import css from "./NotePreview.module.css";
+
+interface Props {
+  noteId: string;
+}
+
+export default function NotePreview({ noteId }: Props) {
+  const router = useRouter();
+
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery<Note>({
+    queryKey: ["note", noteId],
+    queryFn: () => fetchNoteById(noteId),
+  });
+
+  const handleClose = () => {
+    router.back();
+  };
+
+  if (isLoading) {
+    return (
+      <Modal onClose={handleClose}>
+        <div className={css.loading}>Loading...</div>
+      </Modal>
+    );
+  }
+
+  if (error || !note) {
+    return (
+      <Modal onClose={handleClose}>
+        <div className={css.error}>Error loading note.</div>
+      </Modal>
+    );
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+      <div className={css.container}>
+        <div className={css.tag}>{note.tag}</div>
+
+        <h2 className={css.title}>{note.title}</h2>
+
+        <p className={css.content}>{note.content}</p>
+
+        <p className={css.date}>{new Date(note.createdAt).toLocaleString()}</p>
+
+        <button className={css.closeBtn} onClick={handleClose}>
+          ← Back
+        </button>
+      </div>
+    </Modal>
+  );
+}
---
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { fetchNoteById } from "@/lib/api";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import NotePreview from "./NotePreview.client";
+
+type Props = {
+  params: { id: string };
+};
+
+export default async function NotePreviewPage({ params }: Props) {
+  const { id } = params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePreview noteId={id} />
+    </HydrationBoundary>
+  );
+}
